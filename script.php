eb1442ba31eb1784465eb0f57dce70342fb57106
Yago Menezes <tenn.menezes@gmail.com>
2025-08-27 15:17:38 -0300

    first connection on DB

--- script.php
+++ script.php
@@ -1,3 +1,19 @@
 <?php 
-    echo "hello world";
+    // Conexão com MySQL
+    $servername = "127.0.0.1";
+    $username = "yago";
+    $password = "senha123";
+    $database = "mysql";
+
+    // Criar conexão
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Checar conexão
+    if ($conn->connect_error) {
+        die("Conexão falhou: " . $conn->connect_error);
+    }
+    echo "Conexão bem-sucedida!";
+
+    // Fechar conexão
+    $conn->close();
 ?>
